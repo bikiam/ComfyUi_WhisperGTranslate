cf00ffc412d02c516b1afe5550160a9ea8a0da01
bikiam <157801122+bikiam@users.noreply.github.com>
2025-07-02 00:07:28 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,12 @@ from .Whisper_AudioTranslate import WhisperAudioTranslateNode
 from .Google_translate import GoogleTranslateNode
 # Node registration
 NODE_CLASS_MAPPINGS = {
-    "WhisperAudioTranslateNode": WhisperAudioTranslateNode
+    "WhisperAudioTranslateNode": WhisperAudioTranslateNode,
     "GoogleTranslateNode": GoogleTranslateNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "WhisperAudioTranslateNode": "Whisper + AudioTranslate"
+    "WhisperAudioTranslateNode": "Whisper + AudioTranslate",
     "GoogleTranslateNode": "Google Translate Node"
 }
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
